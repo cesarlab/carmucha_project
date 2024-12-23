cffacc0791e070515f3aeed965dcd4d2a636f7cb
cesar <cesar.labrador87@gmail.com>
2024-12-23 10:09:39 -0300

    reinicializando repositorio

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const usuarioBD = "cesar"
+/*const usuarioBD = "cesar"
 const passBD = "test123"
 
 var usuario = prompt("Ingresa tu usuario")
@@ -9,3 +9,54 @@ if((usuario==usuarioBD)&&(pass==passBD)){
 }else{
     document.write("Usuario o contraseña estan mal :/")
 }
+*/
+
+
+/*
+//Calculo del precio en base al pais
+//Esta es una funcionalidad que voy a implementar pero por tema de tiempo no lo voy a incluir en esta entrega final, pero que si voy a seguir trabajando para incluirla en el proyecto y lanzarlo a producción pronto
+let priceUSD = 50
+//Pegandole a la API de $ en latinoamerica
+
+//Chile
+fetch('https://dolarapi.com/v1/dolares/oficial')
+  .then(responseCLP => responseCLP.json())
+  .then(dataCLP =>{console.log(dataCLP)
+                    console.log(dataCLP.venta)
+})
+
+  function priceConvertion(priceUSD, priceCoin ){
+let result = priceUSD * priceCoin
+    console.log(result)
+    return result
+  }
+*/
+
+  //----TIENDA----- Crear tarjetas de productos en base a la información de los objetos 'producto' desde un JSON guardado localmente
+
+let productsContainer = document.getElementById("tienda")
+console.log(productsContainer)
+
+fetch('./products.json')
+.then(response => response.json())
+.then(data => {
+    data.forEach((elemento) => {
+        
+        const productContainerCreated = document.createElement("div")
+        productContainerCreated.classList.add("card-container")
+        productContainerCreated.innerHTML = `
+        <div class="card">
+        <img src="${elemento.image}">
+        <div class="card-body">
+                        <h5 class="card-title">${elemento.name}</h5>
+                        <p class="card-text">${elemento.symbol + elemento.price}</p>
+                        <a href="#contacto" class="btn">Comprar</a>
+        </div>
+        `      
+        
+        
+        ;
+        productsContainer.append(productContainerCreated)
+    })
+
+})
